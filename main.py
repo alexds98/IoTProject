bfc151ddd86f6ad834e762fe908ce4fb741246e4
alexds98 <47061045+alexds98@users.noreply.github.com>
2020-07-09 12:05:17 +0200

    buzzer alarm

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 
 import streams
 import adc
+import pwm
 
 streams.serial()
 
@@ -13,6 +14,11 @@ pinMode(alarm_led_pin, OUTPUT)
 # set potentiometer for analog read
 pot_pin = A4
 pinMode(pot_pin, INPUT_ANALOG)
+
+# set buzzer for pwm
+buzzer_pin = D22
+buzzer_pwm_pin = D22.PWM
+pinMode(buzzer_pin, OUTPUT)
 
 def blink(pin, time_on, time_off) :
         digitalWrite(pin,HIGH)   # turn the led ON by making the voltage HIGH
@@ -40,5 +46,27 @@ def pot_led_thread(led_pin, pot_pin) :
         if stop :
             break
         
+def buzzer_alarm(pwm_pin) :
+    INITIAL_FREQUENCY = 800
+    VARIATION = 39
+    frequency = INITIAL_FREQUENCY
+    mode = 1
+    global stop
+    while True :
+        period = 1000000//frequency
+        pwm.write(pwm_pin, period, period//2, MICROS)
+        if mode == 1 :
+            frequency += 4
+        else :
+            frequency -= 4
+        if frequency < INITIAL_FREQUENCY - VARIATION :
+            mode = 1
+        elif frequency > INITIAL_FREQUENCY + VARIATION :
+            mode = 0
+        if stop :
+            break
+        sleep(25)
+        
 stop = False
 thread(pot_led_thread, alarm_led_pin, pot_pin)
+thread(buzzer_alarm, buzzer_pwm_pin)
